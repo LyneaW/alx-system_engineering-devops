7c5e75de0db0b4e32b97edf743efebfe10677112
LyneaW <wangecilynea@gmail.com>
2024-01-18 07:58:14 -0800

    Files

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+#Apache 500 error in wordpress
+exec { 'fix':
+  onlyif  => 'test -e /var/www/html/wp-settings.php',
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
