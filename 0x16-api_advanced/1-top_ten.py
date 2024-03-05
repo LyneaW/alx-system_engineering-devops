b334a740ad12637ec77c023459917f822b0db72e
LyneaW <wangecilynea@gmail.com>
2024-03-05 13:52:45 -0800

    API Advanced

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,15 +3,16 @@
 
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    to the subreddit"""
-    import requests
+"""Queries the Reddit API and returns the number of subscribers
+to the subreddit"""
+import requests
 
-    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 300:
-        return 0
+sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+.format(subreddit),
+headers={"User-Agent": "My-User-Agent"},
+allow_redirects=False)
+if sub_info.status_code >= 300:
+return 0
 
-    return sub_info.json().get("data").get("subscribers")
+return sub_info.json().get("data").get("subscribers")
+
